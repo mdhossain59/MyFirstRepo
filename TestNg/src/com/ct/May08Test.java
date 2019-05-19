25c4a62ea46bbe2eab91a5abf5a4a2062b31dd3f
BruteForce Solutions <BruteForce Solutions@10.66.106.218>
2019-05-19 10:48:03 -0400

    Added a TestNG Class

---
+++ TestNg/src/com/ct/May08Test.java
@@ -0,0 +1,42 @@
+package com.ct;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+
+public class May08Test {
+  
+	@Test
+  public void TestCase01() {
+		System.out.println("Executing Test Case 01");
+  }
+	
+	@Test(enabled=false)
+	public void TestCase02() {
+		System.out.println("Executing Test Case 02");
+		Assert.assertEquals(true, true);
+		System.out.println("This a Test");
+	}
+	
+	@Test
+	public void TestCase03() {
+		System.out.println("Executing Test Case 03");
+		Assert.assertEquals(150, 170);
+		
+	}
+  
+  
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("Broser opening ................................");
+	  
+  }
+
+  
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("Close Browser xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+  }
+
+}
